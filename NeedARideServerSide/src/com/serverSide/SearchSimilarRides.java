8f51e9a7fa5c278a5809d8a314cd642cb5ea835a
ilantc <ilantch@gmail.com>
2014-02-27 17:23:42 +0200

    adding all fields, saving them in an enum for future use
    updating the check function - splitting it to two functions for easier maintainability

--- NeedARideServerSide/src/com/serverSide/SearchSimilarRides.java
+++ NeedARideServerSide/src/com/serverSide/SearchSimilarRides.java
@@ -1,12 +1,11 @@
 package com.serverSide;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.jsp.ah.datastoreViewerBody_jsp;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
@@ -20,39 +19,80 @@ import com.google.appengine.api.datastore.Query.FilterPredicate;
 @SuppressWarnings("serial")
 public class SearchSimilarRides extends HttpServlet{
 	
+	public static enum rideFields {
+		fromCity, fromLat, fromLng, fromStreet, fromHouseNo,
+		toCity, toLat, toLng, toStreet, toHouseNo,
+		date, sits, fee, driverId, comment;
+		
+		// The DB field name
+		public String toString(rideFields rf) {
+			switch (rf) {	
+			case fromCity: return "fromCity";
+			case fromLat: return "fromLat";
+			case fromLng: return "fromLng";
+			case fromStreet: return "fromStreet";
+			case fromHouseNo: return "fromHouseNo";
+			case toCity: return "toCity";
+			case toLat: return "toLat";
+			case toLng: return "toLng";
+			case toStreet: return "toStreet";
+			case toHouseNo: return "toHouseNo";
+			case date: return "date";
+			case sits: return "availableSits";
+			case fee: return "rideFee";
+			case driverId: return "userID";
+			case comment: return "driversComment";
+			}
+			return ""; // we'll never get here
+		}
+	};
+
+	
 	//handling to income request
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException{
 		
+		HashMap<rideFields, String> driverFields = new HashMap<rideFields, String>(); 
 		//getting the known parameters
-		String from = req.getParameter("fromCity");
-		String to = req.getParameter("toCity");
+		for(rideFields f: rideFields.values()) {
+			if (req.getParameter(f.toString()) != null) {
+				driverFields.put(f, req.getParameter(f.toString()));
+			}
+		}
 		
 		//write back to the user the output of the function "check" as response
-		resp.getWriter().println(check(from,to));
+		resp.getWriter().println(check(driverFields));
 	}
 	
-	public String check(String fromCity, String toCity){
+	public String check(HashMap<rideFields, String> fields){
 		
 		//set the datastore mechanism for searching the request values
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		
-		//set a filter for the "from" property
-		Filter filterFrom = new FilterPredicate("fromCity",FilterOperator.IN,fromCity);
-		
-		//set a filter for the "to" property
-		Filter filterTo = new FilterPredicate("toCity",FilterOperator.IN,toCity);
-		
-		//combine the to filters with "and" operator
-		Filter filterFromAndTo = CompositeFilterOperator.and(filterFrom,filterTo);
+		// get the filter
+		Filter similarRidesFilter = getFilter(fields);
 		
 		//set a new query
-		Query q = new Query("Ride").setFilter(filterFromAndTo);
+		Query q = new Query("Ride").setFilter(similarRidesFilter);
 		
 		//execute the query
 		PreparedQuery pq = datastore.prepare(q);
 		
 		//return the corresponding result
 		return pq.asIterator().hasNext() ? "Found ride" : "Did not find ride";
+	}
+	
+	private Filter getFilter(HashMap<rideFields, String> fields) {
+
+		//set a filter for the "fromCity" property
+		Filter filterFrom = new FilterPredicate(rideFields.fromCity.toString(),FilterOperator.IN,fields.get(rideFields.fromCity));
+		
+		//set a filter for the "to" property
+		Filter filterTo = new FilterPredicate(rideFields.toCity.toString(),FilterOperator.IN,fields.get(rideFields.toCity));
+		
+		//combine the to filters with "and" operator
+		Filter filterFromAndTo = CompositeFilterOperator.and(filterFrom,filterTo);
+		
+		return filterFromAndTo;
 	}
 
 }
